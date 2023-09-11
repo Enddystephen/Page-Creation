0649458f49b6ab6260772ef1d09414d1d0b41050
Udeh Faith <enddystephen@gmail.com>
2023-09-11 08:12:12 +0100

    Page-Creation

---
+++ index.js
@@ -0,0 +1,30 @@
+// Switch method//
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+document.getElementById("day").innerHTML = "Today is: " + day;
+
+// UTC Time in milliseconds//
+const d = new Date();
+document.getElementById("time").innerHTML = "Current UTC Time: " + d.getTime();
